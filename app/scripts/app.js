922b65e78e23deb556f88211704e3cfb5654e936
Arielle Nguyen <ariellephan@gmail.com>
2013-08-02 14:53:22 -0700

    Yeoman Grunt Bower app with jQuery, jCarousel, requirejs, normalize, modernizr, SASS and responsive design

---
+++ app/scripts/app.js
@@ -0,0 +1,6 @@
+/*global define */
+define([], function () {
+    'use strict';
+
+    return 'Responsive jCarousel';
+});
---
+++ app/scripts/main.js
@@ -0,0 +1,15 @@
+require.config({
+    paths: {
+        jquery: '../bower_components/jquery/jquery',
+        jcarousel: '../bower_components/jcarousel/dist/jquery.jcarousel'
+    }
+});
+
+require(['app', 'jquery', 'jcarousel'], function (app, $) {
+    'use strict';
+    // exclude Modernizr because it needs to be loaded before html elements
+    console.log(app);
+    console.log('Running requireJS %s', requirejs.version);
+    console.log('Running jQuery %s', $().jquery);
+    console.log('Running jCarousel %s', $.jCarousel.version);
+});
