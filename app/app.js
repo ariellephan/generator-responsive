8754b8334afd47911568b75a65f3d24d7368fe57
Arielle Nguyen <ariellephan@gmail.com>
2013-08-05 12:05:22 -0700

    Clean up

--- app/app.js
+++ app/app.js
@@ -1,5 +1,7 @@
+'use strict';
+
 define(['jquery', 'jcarousel'], function($) {
-    $(document).ready(function() {
+    $(function() {
 		$('#myjcarousel').jcarousel({
 			wrap: 'circular'
 		});
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,8 @@
 require.config({
-    baseUrl: 'bower_components',
     paths: {
-        jquery: '../bower_components/jquery/jquery',
-        jcarousel: '../bower_components/jcarousel/dist/jquery.jcarousel',
-        app: '../app'
+        app: '../app',
+        jquery: 'jquery-1.9.1.min',
+        jcarousel: 'jquery.jcarousel'
     },
     shim: {
         jcarousel: ['jquery'],
